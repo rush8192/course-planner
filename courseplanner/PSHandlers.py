fbd7099e524bb35575eb9c3d5f220a67b348473e
Kevin Shin <kevinshin92@gmail.com>
2014-05-19 15:14:20 -0700

    Reorganized format of json

--- courseplanner/PSHandlers.py
+++ courseplanner/PSHandlers.py
@@ -21,8 +21,10 @@ class ProgramSheetHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write(get_program_sheet(ps_name = self.request.get('ps_name')))
     def post(self):
-        self.response.write(add_program_sheet(ps_name = self.request.get('ps_name')))
-    def patch(self):
+        ps_name = self.request.get('ps_name')
+        req_box_array = json.loads(self.request.get('req_boxes'))
+        self.response.write(add_program_sheet(ps_name, req_box_array))
+    def put(self):
         ps_key = self.request.get('ps_key')
         new_ps_name = self.request.get('ps_name')
         self.response.write(edit_program_sheet(ps_key, new_ps_name))
@@ -31,77 +33,43 @@ class ProgramSheetHandler(webapp2.RequestHandler):
         self.response.write(remove_program_sheet(ps_key))
 
 class ReqBoxHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(get_program_sheet(rb_key = self.request.get('rb_key')))
+    # Note POST is always called with ps_key
     def post(self):
         ps_key = self.request.get('ps_key')
-        req_box_name = self.request.get('req_box_name')
-        min_units = __str_to_int(self.request.get('min_units'))
-        min_num_courses = __str_to_int(self.request.get('min_num_courses'))
-        conditional_ops = self.request.get('conditional_ops')
-        if conditional_ops == '':
-          conditional_ops = None
-        self.response.write(add_req_box_to_ps(ps_key, req_box_name, \
-                                              min_units, min_num_courses, \
-                                              conditional_ops))
-    def patch(self):
+        req_box_dict = json.loads(self.request.get('req_box'))
+        self.response.write(add_req_box_to_ps(ps_entity=None, ps_key=ps_key, \
+                                              req_box_dict=req_box_dict))
+    def put(self):
         rb_key = self.request.get('rb_key')
-        req_box_name = self.request.get('req_box_name')
-        if req_box_name == '':
-          req_box_name = None
-
-        min_units = self.request.get('min_units')
-        if min_units == '':
-            min_units = None
-        else:
-            min_units = __str_to_int(min_units)
-
-        min_num_courses = self.request.get('min_num_courses')
-        if min_num_courses == '':
-            min_num_courses = None
-        else:
-            __str_to_int(min_num_courses)
-        conditional_ops = self.request.get('conditional_ops')
-        if conditional_ops == '':
-          conditional_ops = None
-        self.response.write(edit_req_box_in_ps(rb_key, req_box_name, \
-                                              min_units, min_num_courses, \
-                                              conditional_ops))
+        req_box_dict = self.request.get('req_box_dict')
+        for key in req_box_dict:
+            if req_box_dict[key] == '':
+                req_box_dict[key] = None
+        self.response.write(edit_req_box_in_ps(rb_entity=None, rb_key=rb_key, \
+                                               req_box_dict=req_box_dict))
     def delete(self):
         rb_key = self.request.get('rb_key')
         self.response.write(remove_req_box_from_ps(rb_key))
 
 class ReqCourseHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(get_program_sheet(rc_key = self.request.get('rc_key')))
+    # Note POST is always called with rb_key
     def post(self):
         rb_key = self.request.get('rb_key')
-        req_course_name = self.request.get('req_course_name')
-        min_units = __str_to_int(self.request.get('min_units'))
-        min_grade = __str_to_float(self.request.get('min_grade'))
-        allowed_courses = self.request.get('allowed_courses_ops')
-        self.response.write(add_req_course_to_rb(rb_key, req_course_name, \
-                                                 min_units, min_grade, \
-                                                 allowed_courses))
-    def patch(self):
+        req_course_dict = json.loads(self.request.get('req_course'))
+        self.response.write(add_req_course_to_rb(rb_entity=None, rb_key=rb_key, \
+                                                 req_course_dict=req_course_dict))
+    def put(self):
         rc_key = self.request.get('rc_key')
-        req_course_name = self.request.get('req_course_name')
-        if req_course_name == '':
-          req_course_name = None
-
-        min_units = self.request.get('min_units')
-        if min_units == '':
-            min_units = None
-        else:
-            min_units = __str_to_int(min_units)
-
-        min_grade = self.request.get('min_grade')
-        if min_grade == '':
-            min_grade = None
-        else:
-            __str_to_float(min_grade)
-        allowed_courses = self.request.get('allowed_courses')
-        if allowed_courses == '':
-          allowed_courses = None
-        self.response.write(edit_req_course_in_rb(rc_key, req_course_info, \
-                                                  min_units, min_grade, \
-                                                  allowed_courses))
+        req_course_dict = json.loads(self.request.get('req_course_dict'))
+        for key in req_course_dict:
+            if req_course_dict[key] == '':
+                req_course_dict[key] = None
+        self.response.write(edit_req_course_in_rb(rc_entity=None, rc_key=rc_key, \
+                                                  req_course_dict=req_course_dict))
     def delete(self):
         rc_key = self.request.get('rc_key')
         self.response.write(remove_req_course_from_rb(rc_key))
--- courseplanner/ops.py
+++ courseplanner/ops.py
@@ -52,12 +52,52 @@ def __program_sheet_exists(ps_name):
         return False
     return True
 
-# Checks existence of program sheet, true/false
-def __get_ps_entity(ps_name):
-    sheets = Program_Sheet.query(Program_Sheet.ps_name == ps_name).fetch(1)
-    if len(sheets) > 0:
-        return sheets[0]
-    return None
+# Converts Req_Course entity to dictionary, replaces allowed course keys
+# with course names
+def __get_req_course_dict(rc_key):
+    rc_entity = __entity_from_key_str(rc_key)
+    if rc_entity is None:
+        return 'Cannot find Req_Course'
+    rc_dict = rc_entity.to_dict()
+    allowed_course_keys = list(rc_dict['allowed_courses'])
+    rc_dict['allowed_courses'] = []
+    for ac_key in allowed_course_keys:
+        ac_entity = __entity_from_key_str(ac_key)
+        if ac_entity is None:
+            return 'Cannot find course from allowed course list'
+        rc_dict['allowed_courses'].append(ac_entity.course_num)
+    return rc_dict
+
+# Converts Req_Box entity to dictionary with Req_Courses filled in
+def __get_req_box_dict(rb_key):
+    rb_entity = __entity_from_key_str(rb_key)
+    if rb_entity is None:
+        return 'Cannot find Req_Box'
+    rb_dict = rb_entity.to_dict()
+    req_course_keys = list(rb_dict['req_courses'])
+    rb_dict['req_courses'] = []
+    for rc_key in req_course_keys:
+        req_course_dict = __get_req_course_dict(rc_key)
+        if type(req_course_dict) != dict:
+            return req_course_dict
+        rb_dict['req_courses'].append(req_course_dict)
+    return rb_dict
+
+# Converts Program_Sheet to dictionary with Req_Boxes filled in
+def __get_ps_dict(ps_name):
+    ps = Program_Sheet.query(Program_Sheet.ps_name == ps_name).fetch(1)
+    if len(ps == 0):
+        return 'Program Sheet does not exist'
+    ps_entity = ps[0]
+    ps_dict = ps_entity.to_dict()
+    req_box_keys = list(ps_dict['req_boxes'])
+    ps_dict['req_boxes'] = []
+    for rb_key in req_box_keys:
+        req_box_dict = __get_req_box_dict(rb_key)
+        if type(req_box_dict) != dict:
+            return req_box_dict
+        ps_dict['req_boxes'].append(req_box_dict)
+    return ps_dict
 
 # Fetches course entity
 # Returns entity if found, or None
@@ -242,12 +282,17 @@ Returns:
     True on success
     Error message on failure
 """
-def add_program_sheet(ps_name):
+def add_program_sheet(ps_name, req_box_array):
     if (__program_sheet_exists(ps_name)):
         return ERROR('Program sheet already exists for ' + str(ps_name) + '.')
-    ps = Program_Sheet(ps_name=ps_name)
-    ps.put()
-    return True
+    ps_entity = Program_Sheet(ps_name=ps_name)
+    for req_box in req_box_array:
+        result = add_req_box_to_ps(ps_entity, req_box)
+        # Exists Error, Abort
+        if (result != True):
+            return result
+    ps_entity.put()
+    return result
 
 """
 Fetches Program Sheet based on name (exact match)
@@ -257,10 +302,10 @@ Returns:
     Error message on failure
 """
 def get_program_sheet(ps_name):
-    ps_entity =__get_ps_entity(ps_name)
-    if ps_entity is None:
-        return ERROR('Program sheet named ' + str(ps_name) + ' not found.')
-    return json.dumps(ps_entity.to_dict())  
+    ps_dict = __get_ps_dict(ps_name)
+    if type(ps_dict) != dict:
+        return ERROR(ps_dict)
+    return json.dumps(ps_dict)
 
 """
 Edits program sheet name given key to program sheet
@@ -284,6 +329,9 @@ def remove_program_sheet(ps_key):
     ps_entity = __entity_from_key_str(ps_key, 'Program_Sheet')
     if ps_entity is None:
         return
+    req_boxes_copy = list(ps_entity.req_boxes)
+    for req_box_key in req_boxes_copy:
+        remove_req_box_from_ps(req_box_key)
     ps_entity.key.delete()
 
 """
@@ -297,22 +345,42 @@ Returns:
     True on success
     Error message on failure
 """
-def add_req_box_to_ps(ps_key, req_box_name, min_units, min_num_courses, \
-                      conditional_ops = None):
-    ps_entity = __entity_from_key_str(ps_key, 'Program_Sheet')
-    if ps_entity is None: # None if doesn't exist?
-        return ERROR('Program sheet does not currently exist.')
-    min_units = int(min_units)
-    min_num_courses = int(min_num_courses)
+def add_req_box_to_ps(ps_entity, req_box_dict, ps_key=None):
+    if ps_key != None:
+        ps_entity = __entity_from_key_str(ps_key, 'Program_Sheet')
+        if ps_entity is None: # None if doesn't exist?
+            return ERROR('Program sheet does not currently exist.')
+    req_box_name = req_box_dict['req_box_name']
+    min_units = req_box_dict['min_total_units']
+    min_num_courses = req_box_dict['min_num_courses']
+    conditional_ops = req_box_dict['conditional_ops']
+    rb_entity = Req_Box(program_sheet=ps_entity, req_box_name=req_box_name, \
+                      min_total_units=min_units, min_num_courses=min_num_courses, \
+                      conditional_ops=conditional_ops)
 
-    req_box_entity = Req_Box(program_sheet=ps_entity, req_box_name=req_box_name, \
-                      min_total_units=min_units, min_num_courses=min_num_courses)
-    if (conditional_ops != None):
-        req_box.conditional_ops = conditional_ops
-    req_box_entity.put()
+    req_course_array = req_box_dict['req_courses']
+    for req_course in req_course_array:
+        result = add_req_course_to_rb(rb_entity, req_course)
+        # Exists Error, Abort
+        if (result != True):
+            return result
+    rb_entity.put()
     ps_entity.req_boxes.append(req_box_entity.key())
     ps_entity.put()
     return True
+
+"""
+Fetches Required Box based on key (exact match)
+
+Returns:
+    Json dump on success
+    Error message on failure
+"""
+def get_req_box(rb_key):
+    rb_dict = __get_rb_dict(rb_key)
+    if rb_dict is None:
+        return ERROR('Required Box not found.')
+    return json.dumps(rb_dict)
 
 """
 Edits a Req_Box in a Program_Sheet
@@ -325,11 +393,14 @@ Returns:
     True on success
     Error message on failure
 """
-def edit_req_box_in_ps(rb_key, req_box_name=None, min_units=None, \
-                       min_num_courses=None, conditional_ops=None):
+def edit_req_box_in_ps(rb_entity, req_box_dict, rb_key=None):
     rb_entity = __entity_from_key_str(rb_key, 'Req_Box') 
     if rb_entity is None: # None if doesn't exist?
         return ERROR('Req box does not currently exist.')
+    req_box_name = req_box_dict['req_box_dict']
+    min_units = req_box_dict['min_units']
+    min_num_courses = req_box_dict['min_num_courses']
+    conditional_ops = req_box_dict['conditional_ops']
     if req_box_name != None:
         rb_entity.req_box_name = req_box_name
     if min_units != None:
@@ -356,6 +427,9 @@ def remove_req_box_from_ps(rb_key):
                     del ps_entity.req_boxes[i]
                     break
             ps_entity.put()
+        req_courses_copy = list(rb_entity.req_courses)
+        for req_course_key in req_courses_copy:
+            remove_req_course_from_rb(req_course_key)
         rb_entity.key.delete()
 
 """
@@ -369,26 +443,40 @@ Returns:
     True on success
     Error message on failure
 """
-def add_req_course_to_rb(rb_key, req_course_info, \
-                         min_units, min_grade, allowed_courses):
-    req_box_entity = __entity_from_key_str(rb_key, 'Req_Box')
-    if req_box_entity is None: # None if doesn't exist?
-        return ERROR('Required box does not currently exist.')
-    min_units = int(min_units)
-    min_grade = float(min_grade)
-    allowed_courses = allowed_courses.split(',').strip()
-    
-    req_course_entity = Req_Course(req_box=req_box_entity, req_course_info=req_course_info, \
-                                   min_units = min_units, min_grade=min_grade)
+def add_req_course_to_rb(rb_entity, req_course_dict, rb_key = None):
+    if rb_key != None:
+        rb_entity = __entity_from_key_str(rb_key, 'Req_Box')
+        if rb_entity is None: # None if doesn't exist?
+            return ERROR('Required box does not currently exist.')
+    req_course_info = req_course_dict['req_course_info']
+    min_units = req_course_dict['min_units']
+    min_grade = req_course_dict['min_grade']
+    allowed_courses = req_course_dict['allowed_courses']
+
+    rc_entity = Req_Course(req_box=rb_entity, req_course_info=req_course_info, \
+                           min_units = min_units, min_grade=min_grade)
     for course_name in allowed_courses:
         course_listing = __get_course_listing_entity(course_name)
         if course_listing == None:
             return ERROR('Course ' + course_name + ' does not exist')
-        req_course_entity.allowed_courses.append(course_listing.key())
-    req_course_entity.put()
-    req_box_entity.req_courses.append(req_course_entity.key())
-    req_box_entity.put()
+        rc_entity.allowed_courses.append(course_listing.key())
+    rc_entity.put()
+    rb_entity.req_courses.append(rc_entity.key())
+    rb_entity.put()
     return True
+
+"""
+Fetches Required Course based on key (exact match)
+
+Returns:
+    Json dump on success
+    Error message on failure
+"""
+def get_req_course(rc_key):
+    rc_dict = __get_rc_dict(rc_key)
+    if rc_dict is None:
+        return ERROR('Required course not found.')
+    return json.dumps(rc_dict)
 
 """
 Adds a Req_Course to link to a Req_Box
@@ -400,18 +488,23 @@ Returns:
     True on success
     Error message on failure
 """
-def edit_req_course_in_rb(rc_key, req_course_info=None, \
-                          min_units=None, min_grade=None, allowed_courses=None):
-    rc_entity = __entity_from_key_str(rc_key, 'Req_Course')
-    if rc_entity is None: # None if doesn't exist?
-        return ERROR('Required course does not currently exist.')
+def edit_req_course_in_rb(rc_entity, req_course_dict, rc_key = None):
+    if (rc_key is not None):
+        rc_entity = __entity_from_key_str(rc_key, 'Req_Course')
+        if rc_entity is None: # None if doesn't exist?
+            return ERROR('Required course does not currently exist.')
+    req_course_info = req_course_dict['req_course_dict']
+    min_units = req_course_dict['min_units']
+    min_grade = req_course_dict['min_grade']
+    allowed_courses = req_course_dict['allowed_courses']
     if req_course_info:
         rc_entity.req_course_info = req_course_info
     if min_grade:
         rc_entity.min_grade = float(min_grade)
+    if min_units:
+        rc_entity.min_units = int(min_units)
     if allowed_courses:
         del rc_entity.allowed_courses[:]
-        allowed_courses = allowed_courses.split(',').strip()
         for course_name in allowed_courses:
             course_listing = __get_course_listing_entity(course_name)
             if course_listing == None:
